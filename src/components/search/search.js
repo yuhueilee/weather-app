43dc90b17a339fd2995b3dcfa0a64cd463e17819
yuhueilee <wendylee70127@gmail.com>
2024-01-16 19:08:06 +0800

    Change function into asynchronous

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -5,22 +5,20 @@ import { geoApiOptions, GEO_API_URL } from "../../api";
 const Search = ({ onSearchChange }) => {
   const [search, setSearch] = useState(null);
 
-  const loadOptions = (inputValue) => {
-    return fetch(
+  const loadOptions = async (inputValue) => {
+    const response = await fetch(
       `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`,
       geoApiOptions
-    )
-      .then((response) => response.json())
-      .then((response) => {
+    ).then((response) => response.json());
+    
+    return {
+      options: response.data.map((city) => {
         return {
-          options: response.data.map((city) => {
-            return {
-              value: `${city.latitude} ${city.longitude}`,
-              label: `${city.name}, ${city.countryCode}`,
-            };
-          }),
+          value: `${city.latitude} ${city.longitude}`,
+          label: `${city.name}, ${city.countryCode}`,
         };
-      });
+      }),
+    };
   };
 
   const handleOnChange = (searchData) => {
