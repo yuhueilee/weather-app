83c0e2d9fe1087824c91e7ee7d598d3b91e14468
yuhueilee <wendylee70127@gmail.com>
2024-03-06 10:56:36 +0800

    Add wrapper for text

--- src/components/weather/weather.js
+++ src/components/weather/weather.js
@@ -4,8 +4,10 @@ const weather = () => {
     return (
         <div className="weather">
             <div className="top">
-                <p className="city">London</p>
-                <p className="description">Rainy</p>
+                <div>
+                    <p className="city">London</p>
+                    <p className="description">Rainy</p>
+                </div>
                 <img
                     alt="weather"
                     className="weatherIcon"
