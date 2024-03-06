ba5f9b1147bc9cef78afb98c023b63f23708c9e3
yuhueilee <wendylee70127@gmail.com>
2024-03-06 10:33:30 +0800

    Format code

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
-export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 export const geoApiOptions = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
-	}
+    method: "GET",
+    headers: {
+        "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+        "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
 };
