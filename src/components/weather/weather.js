e3697e75125c0621cdc84cd8cbc96c070f66db6c
yuhueilee <wendylee70127@gmail.com>
2024-03-06 10:48:06 +0800

    Add static data and source the icon image

--- src/components/weather/weather.js
+++ src/components/weather/weather.js
@@ -1,7 +1,15 @@
 import "./weather.scss";
 
 const weather = () => {
-    return "hello";
+    return (
+        <div className="weather">
+            <div className="top">
+                <p className="city">London</p>
+                <p className="description">Rainy</p>
+            </div>
+            <img alt="weather" className="weatherIcon" src="icons/01d.png" />
+        </div>
+    );
 };
 
 export default weather;
