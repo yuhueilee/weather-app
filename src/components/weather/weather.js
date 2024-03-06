4125b7c53c1a37f0179d3726289454b3ce836b9d
yuhueilee <wendylee70127@gmail.com>
2024-03-06 11:09:55 +0800

    Add details static data

--- src/components/weather/weather.js
+++ src/components/weather/weather.js
@@ -14,6 +14,30 @@ const weather = () => {
                     src="icons/01d.png"
                 />
             </div>
+            <div className="bottom">
+                <p className="temperature">18C</p>
+                <div className="details">
+                    <div className="parameterRow">
+                        <span className="parameterLabel">Details</span>
+                    </div>
+                    <div className="parameterRow">
+                        <span className="parameterLabel">Feels like</span>
+                        <span className="parameterValue">22C</span>
+                    </div>
+                    <div className="parameterRow">
+                        <span className="parameterLabel">Wind</span>
+                        <span className="parameterValue">2 m/s</span>
+                    </div>
+                    <div className="parameterRow">
+                        <span className="parameterLabel">Humidity</span>
+                        <span className="parameterValue">15%</span>
+                    </div>
+                    <div className="parameterRow">
+                        <span className="parameterLabel">Pressure</span>
+                        <span className="parameterValue">15 hPa</span>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
