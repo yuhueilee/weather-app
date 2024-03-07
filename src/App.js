d15ff443eba236182cee5b33159a22bd7698af3a
yuhueilee <wendylee70127@gmail.com>
2024-03-07 15:00:31 +0800

    Add api calls to get current weather and forecast data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
+import { useState } from "react";
 import "./App.css";
+import { WEATHER_API_URL } from "./api";
 import Search from "./components/search/search";
 import CurrentWeather from "./components/weather/weather";
 
 function App() {
+    const [currentWeather, setCurrentWeather] = useState(null);
+    const [forecast, setForecast] = useState(null);
+
     const handleOnSearchChange = (searchData) => {
-        console.log(searchData);
+        const [lat, lon] = searchData.value.split(" ");
+
+        const currentWeatherFetch = fetch(
+            `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_WEATHER_API_KEY}`
+        );
+        const forecastFetch = fetch(
+            `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_WEATHER_API_KEY}`
+        );
+
+        Promise.all([currentWeatherFetch, forecastFetch])
+            .then(async (response) => {
+                const weatherResp = await response[0].json();
+                const forecastResp = await response[1].json();
+
+                setCurrentWeather({ city: searchData.label, ...weatherResp });
+                setForecast({ city: searchData.label, ...forecastResp });
+            })
+            .catch((err) => console.log(err));
     };
+
+    console.log(currentWeather);
+    console.log(forecast);
 
     return (
         <div className="container">
--- src/api.js
+++ src/api.js
@@ -6,3 +6,5 @@ export const geoApiOptions = {
         "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
     },
 };
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
