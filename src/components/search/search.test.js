e15ce4c39ff15c16e029503612b634bd063c9c62
yuhueilee <wendylee70127@gmail.com>
2024-03-13 15:08:28 +0800

    Add test case

--- src/components/search/search.test.js
+++ src/components/search/search.test.js
@@ -1,12 +1,46 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import Search from "./search";
 
 const mockOnSearchChange = jest.fn();
 
+global.fetch = jest.fn(() => {
+    Promise.resolve({
+        json: () =>
+            Promise.resolve({
+                data: [
+                    {
+                        latitude: 12.3,
+                        longitude: 382.2,
+                        name: "London",
+                        countryCode: "GB",
+                    },
+                    {
+                        latitude: 272.3,
+                        longitude: 312.2,
+                        name: "Span",
+                        countryCode: "SP",
+                    },
+                ],
+            }),
+    });
+});
+
 describe("search component", () => {
+    beforeEach(() => {
+        fetch.mockClear();
+    });
+
     it("should display search bar", () => {
         render(<Search onSearchChange={mockOnSearchChange} />);
         const searchElement = screen.getByRole("combobox");
         expect(searchElement).toBeInTheDocument();
+    });
+
+    it("should load option initially", async () => {
+        render(<Search onSearchChange={mockOnSearchChange} />);
+        const searchElement = screen.getByRole("combobox");
+        fireEvent.click(searchElement);
+        const optionElements = await screen.findAllByRole("paragraph");
+        expect(optionElements.length).toBe(2);
     });
 });
