b726a1a5b0fe64a19c59577f9fde178e491fd6bc
yuhueilee <wendylee70127@gmail.com>
2024-03-14 10:32:13 +0800

    Assert on the fetch call times

--- src/components/search/search.test.js
+++ src/components/search/search.test.js
@@ -1,34 +1,12 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import Search from "./search";
 
 const mockOnSearchChange = jest.fn();
 
-// global.fetch = jest.fn(() => {
-//     Promise.resolve({
-//         json: () =>
-//             Promise.resolve({
-//                 data: [
-//                     {
-//                         latitude: 12.3,
-//                         longitude: 382.2,
-//                         name: "London",
-//                         countryCode: "GB",
-//                     },
-//                     {
-//                         latitude: 272.3,
-//                         longitude: 312.2,
-//                         name: "Span",
-//                         countryCode: "SP",
-//                     },
-//                 ],
-//             }),
-//     });
-// });
-
 describe("search component", () => {
-    // beforeEach(() => {
-    //     fetch.mockClear();
-    // });
+    afterEach(() => {
+        jest.resetAllMocks();
+    });
 
     it("should display search bar", () => {
         render(<Search onSearchChange={mockOnSearchChange} />);
@@ -36,11 +14,36 @@ describe("search component", () => {
         expect(searchElement).toBeInTheDocument();
     });
 
-    // it("should load option initially", async () => {
-    //     render(<Search onSearchChange={mockOnSearchChange} />);
-    //     const searchElement = screen.getByRole("combobox");
-    //     fireEvent.focus(searchElement);
-    //     const optionElements = await screen.findAllByRole("option");
-    //     expect(optionElements.length).toBe(2);
-    // });
+    it("should load static option initially", async () => {
+        jest.spyOn(global, "fetch").mockImplementationOnce(() => {
+            return Promise.resolve({
+                json: () =>
+                    Promise.resolve({
+                        data: [
+                            {
+                                latitude: 12.3,
+                                longitude: 382.2,
+                                name: "London",
+                                countryCode: "GB",
+                            },
+                            {
+                                latitude: 272.3,
+                                longitude: 312.2,
+                                name: "Span",
+                                countryCode: "SP",
+                            },
+                        ],
+                    }),
+            });
+        });
+
+        render(<Search onSearchChange={mockOnSearchChange} />);
+        await waitFor(() => {
+            // screen.debug();
+            return expect(global.fetch).toHaveBeenCalledTimes(2);
+        });
+
+        // const optionElements = await screen.findAllByRole("option");
+        // expect(optionElements.length).toBe(2);
+    });
 });
