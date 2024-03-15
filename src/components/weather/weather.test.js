cfbc59151a8dceef9a28cb8d9b4202cfec8d26fc
yuhueilee <wendylee70127@gmail.com>
2024-03-15 14:18:01 +0800

    Return the assertion

--- src/components/weather/weather.test.js
+++ src/components/weather/weather.test.js
@@ -19,48 +19,48 @@ describe("weather component", () => {
     it("should display city name", () => {
         render(<Weather data={mockData} />);
         const cityElement = screen.getByText("London");
-        expect(cityElement).toBeInTheDocument();
+        return expect(cityElement).toBeInTheDocument();
     });
 
     it("should display weather description", () => {
         render(<Weather data={mockData} />);
         const descriptionElement = screen.getByText("Broken cloud");
-        expect(descriptionElement).toBeInTheDocument();
+        return expect(descriptionElement).toBeInTheDocument();
     });
 
     it("should display weather icon", () => {
         render(<Weather data={mockData} />);
         const imageElement = screen.getByAltText("weather");
-        expect(imageElement).toHaveAttribute("src", "icons/04n.png");
+        return expect(imageElement).toHaveAttribute("src", "icons/04n.png");
     });
 
     it("should display rounded temperature", () => {
         render(<Weather data={mockData} />);
         const temperatureElement = screen.getByText("11°C");
-        expect(temperatureElement).toBeInTheDocument();
+        return expect(temperatureElement).toBeInTheDocument();
     });
 
     it("should display rounded feels like", () => {
         render(<Weather data={mockData} />);
         const feelsLikeElement = screen.getByText("12°C");
-        expect(feelsLikeElement).toBeInTheDocument();
+        return expect(feelsLikeElement).toBeInTheDocument();
     });
 
     it("should display wind speed", () => {
         render(<Weather data={mockData} />);
         const windSpeedElement = screen.getByText("4.63 m/s");
-        expect(windSpeedElement).toBeInTheDocument();
+        return expect(windSpeedElement).toBeInTheDocument();
     });
 
     it("should display humidity", () => {
         render(<Weather data={mockData} />);
         const humidityElement = screen.getByText("89%");
-        expect(humidityElement).toBeInTheDocument();
+        return expect(humidityElement).toBeInTheDocument();
     });
 
     it("should display pressure", () => {
         render(<Weather data={mockData} />);
         const pressureElement = screen.getByText("1012 hPa");
-        expect(pressureElement).toBeInTheDocument();
+        return expect(pressureElement).toBeInTheDocument();
     });
 });
