e5bc8dd15899ab68d2121e01f465f367bc262875
yuhueilee <wendylee70127@gmail.com>
2024-03-15 14:47:04 +0800

    Add test case to validate the number of calls for fetch

--- src/components/search/search.test.js
+++ src/components/search/search.test.js
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import Search from "./search";
 
 const mockOnSearchChange = jest.fn();
@@ -18,6 +18,37 @@ describe("search component", () => {
         render(<Search onSearchChange={mockOnSearchChange} />);
         const textElement = screen.getByText("Search for city");
         return expect(textElement).toBeInTheDocument();
+    });
+
+    it("should call fetch once when input value change", async () => {
+        jest.spyOn(global, "fetch").mockImplementationOnce(() => {
+            return Promise.resolve({
+                json: () =>
+                    Promise.resolve({
+                        data: [
+                            {
+                                latitude: 12.3,
+                                longitude: 382.2,
+                                name: "London",
+                                countryCode: "GB",
+                            },
+                            {
+                                latitude: 272.3,
+                                longitude: 312.2,
+                                name: "Lubango",
+                                countryCode: "AO",
+                            },
+                        ],
+                    }),
+            });
+        });
+
+        render(<Search onSearchChange={mockOnSearchChange} />);
+        const searchElement = screen.getByRole("combobox");
+        fireEvent.change(searchElement, { target: { value: "L" } });
+        return await waitFor(() =>
+            expect(global.fetch).toHaveBeenCalledTimes(1)
+        );
     });
 
     it("should load options when entering input", async () => {
