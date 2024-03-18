7dd93deec72b10e9a49bdf5b981b06a0ffc83b4b
yuhueilee <wendylee70127@gmail.com>
2024-03-18 11:24:07 +0800

    Start the msw worker for dev env

--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+async function enableMocking() {
+    if (process.env.NODE_ENV !== "development") {
+        return;
+    }
+
+    const { worker } = await import("./mocks/browser");
+
+    // `worker.start()` returns a Promise that resolves
+    // once the Service Worker is up and ready to intercept requests.
+    return worker.start();
+}
+
+enableMocking().then(() => {
+    const root = ReactDOM.createRoot(document.getElementById("root"));
+    root.render(
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    );
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
