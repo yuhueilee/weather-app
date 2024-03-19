11cbf09980f8fba862908884fe5bb852488063a2
yuhueilee <wendylee70127@gmail.com>
2024-03-19 13:56:21 +0800

    Add test case

---
+++ src/App.test.js
@@ -0,0 +1,17 @@
+import { render, screen } from "@testing-library/react";
+
+import App from "./App";
+import userEvent from "@testing-library/user-event";
+
+describe("app component", () => {
+    it("should weather description after option is selected", async () => {
+        render(<App />);
+        const searchElement = screen.getByRole("combobox");
+        await userEvent.type(searchElement, "London");
+        const optionElement = await screen.findByRole("option", {
+            name: "London, GB",
+        });
+        await userEvent.click(optionElement);
+        return expect(screen.getByText("overcast clouds")).toBeInTheDocument();
+    });
+});
