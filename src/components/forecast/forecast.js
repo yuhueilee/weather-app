059d65f7ab2c664a46225ef1789c38fed83be2d0
yuhueilee <wendylee70127@gmail.com>
2024-03-20 17:47:34 +0800

    Add forecast weather details

--- src/components/forecast/forecast.js
+++ src/components/forecast/forecast.js
@@ -58,7 +58,34 @@ const Forecast = ({ data }) => {
                                 </div>
                             </AccordionItemButton>
                         </AccordionItemHeading>
-                        <AccordionItemPanel></AccordionItemPanel>
+                        <AccordionItemPanel>
+                            <div className="daily-details-grid">
+                                <div className="daily-details-grid-item">
+                                    <label>Pressure:</label>
+                                    <label>{item.main.pressure}</label>
+                                </div>
+                                <div className="daily-details-grid-item">
+                                    <label>Humidity:</label>
+                                    <label>{item.main.humidity}</label>
+                                </div>
+                                <div className="daily-details-grid-item">
+                                    <label>Clouds:</label>
+                                    <label>{item.clouds.all}%</label>
+                                </div>
+                                <div className="daily-details-grid-item">
+                                    <label>Wind speed:</label>
+                                    <label>{item.wind.speed} m/s</label>
+                                </div>
+                                <div className="daily-details-grid-item">
+                                    <label>Sea level:</label>
+                                    <label>{item.main.sea_level}m</label>
+                                </div>
+                                <div className="daily-details-grid-item">
+                                    <label>Feels like:</label>
+                                    <label>{item.main.feels_like}°C</label>
+                                </div>
+                            </div>
+                        </AccordionItemPanel>
                     </AccordionItem>
                 ))}
             </Accordion>
