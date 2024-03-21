7c36f027e96a3a097126e339fe2793e74f7c4ccf
yuhueilee <wendylee70127@gmail.com>
2024-03-21 14:11:28 +0800

    Remove console log

--- src/App.js
+++ src/App.js
@@ -30,9 +30,6 @@ function App() {
             .catch((err) => console.log(err));
     };
 
-    console.log(currentWeather);
-    console.log(forecast);
-
     return (
         <div className="container">
             <Search onSearchChange={handleOnSearchChange} />
